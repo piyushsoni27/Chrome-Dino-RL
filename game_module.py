1e0c0cb5b03da1851b8ec2743a33ba311883ca2c
Piyush Soni <piy1995s@gmail.com>
2018-08-17 14:00:19 +0530

    initial commit

---
+++ game_module.py
@@ -0,0 +1,62 @@
+import numpy as np
+from PIL import ImageGrab, Image
+import cv2
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
+
+#path variables
+game_url = "game/dino.html"
+chrome_driver_path = "./chromedriver.exe"
+#loss_file_path = "./objects/loss_df.csv"
+#actions_file_path = "./objects/actions_df.csv"
+#scores_file_path = "./objects/scores_df.csv"
+
+class Game:
+    
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("disable-infobars")
+        self._driver = webdriver.Chrome(executable_path = chrome_driver_path,chrome_options=chrome_options)
+        self._driver.set_window_position(x=-10,y=0)
+        self._driver.set_window_size(200, 300)
+        self._driver.get(os.path.abspath(game_url))
+    
+    def _isplaying(self):
+        return self._driver.execute_script("return Runner.instance_.playing")
+    
+    def _iscrashed(self):
+        return self._driver.execute_script("return Runner.instance_.crashed")
+    
+    def _restart(self):
+        self._driver.execute_script("Runner.instance_.restart()")
+        
+        time.sleep(0.25)# no actions are possible 
+                        # for 0.25 sec after game starts, 
+                        # skip learning at this time and make the model wait
+                        
+    def _pressUp(self):    
+        self._driver.find_element_by_tag_name("body"),send_keys(Keys.ARROW_UP)
+    
+    def _get_score(self):
+        score_array = self._driver.execute_script("return Runner.instance_.distanceMeter.digits")
+        score = ''.join(score_array) # the javascript object is of type array with score in the formate[1,0,0] which is 100.
+        return int(score)
+        
+    def _pause(self):
+        return self._driver.execute_script("return Runner.instance_.stop()")
+    
+    def _resume(self):
+        return self._driver.execute_script("return Runner.instance_.play()")
+    
+    def _end(self):
+        self._driver.close()
+        
+        
+if __name__ == "__main__":
+    game = Game()
