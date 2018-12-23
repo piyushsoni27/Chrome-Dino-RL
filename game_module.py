f246c97afa1cf29cb1b187b9c8450962de0b4f24
piyushsoni27 <piy1995s@gmail.com>
2018-12-23 23:53:02 +0530

    updated

--- game_module.py
+++ game_module.py
@@ -178,4 +178,210 @@ LEARNING_RATE = 1e-4
 img_rows , img_cols = 40,20
 img_channels = 4 #We stack 4 frames
 
+def buildmodel():
+    print("Now we build the model")
+    model = Sequential()
+    model.add(Conv2D(32, (8, 8), strides=(4, 4), padding='same',input_shape=(img_cols,img_rows,img_channels)))  #20*40*4
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (4, 4), strides=(2, 2), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(ACTIONS))
+    adam = Adam(lr=LEARNING_RATE)
+    model.compile(loss='mse',optimizer=adam)
+    print("We finish building the model")
+    return model
+
+
+''' 
+main training module
+Parameters:
+* model => Keras Model to be trained
+* game_state => Game State module with access to game environment and dino
+* observe => flag to indicate wherther the model is to be trained(weight updates), else just play
+'''
+def trainNetwork(model,game_state,observe=False):
+    last_time = time.time()
+    # store the previous observations in replay memory
+    D = load_obj("D") #load from file system
+    # get the first state by doing nothing
+    do_nothing = np.zeros(ACTIONS)
+    do_nothing[0] =1 #0 => do nothing,
+                     #1=> jump
+    
+    x_t, r_0, terminal = game_state.get_state(do_nothing) # get next step after performing the action
+    
+
+    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2) # stack 4 images to create placeholder input
+    
+
+    
+    s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])  #1*20*40*4
+    
+    initial_state = s_t 
+
+    if observe :
+        OBSERVE = 999999999    #We keep observe, never train
+        epsilon = FINAL_EPSILON
+        print ("Now we load weight")
+        model.load_weights("model_final.h5")
+        adam = Adam(lr=LEARNING_RATE)
+        model.compile(loss='mse',optimizer=adam)
+        print ("Weight load successfully")    
+    else:                       #We go to training mode
+        OBSERVE = OBSERVATION
+        epsilon = load_obj("epsilon") 
+        model.load_weights("model_final.h5")
+        adam = Adam(lr=LEARNING_RATE)
+        model.compile(loss='mse',optimizer=adam)
+
+    t = load_obj("time") # resume from the previous time step stored in file system
+    while (True): #endless running
+        
+        loss = 0
+        Q_sa = 0
+        action_index = 0
+        r_t = 0 #reward at 4
+        a_t = np.zeros([ACTIONS]) # action at t
+        
+        #choose an action epsilon greedy
+        if t % FRAME_PER_ACTION == 0: #parameter to skip frames for actions
+            if  random.random() <= epsilon: #randomly explore an action
+                print("----------Random Action----------")
+                action_index = random.randrange(ACTIONS)
+                a_t[0] = 1
+            else: # predict the output
+                q = model.predict(s_t)       #input a stack of 4 images, get the prediction
+                max_Q = np.argmax(q)         # chosing index with maximum q value
+                action_index = max_Q 
+                a_t[action_index] = 1        # o=> do nothing, 1=> jump
+                
+        #We reduced the epsilon (exploration parameter) gradually
+        if epsilon > FINAL_EPSILON and t > OBSERVE:
+            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE 
+
+        #run the selected action and observed next state and reward
+        x_t1, r_t, terminal = game_state.get_state(a_t)
+        print('loop took {} seconds'.format(time.time()-last_time)) # helpful for measuring frame rate
+        last_time = time.time()
+        x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1) #1x20x40x1
+        s_t1 = np.append(x_t1, s_t[:, :, :, :3], axis=3) # append the new image to input stack and remove the first one
+        
+        
+        # store the transition in D
+        D.append((s_t, action_index, r_t, s_t1, terminal))
+        if len(D) > REPLAY_MEMORY:
+            D.popleft()
+
+        #only train if done observing
+        if t > OBSERVE: 
+            
+            #sample a minibatch to train on
+            minibatch = random.sample(D, BATCH)
+            inputs = np.zeros((BATCH, s_t.shape[1], s_t.shape[2], s_t.shape[3]))   #32, 20, 40, 4
+            targets = np.zeros((inputs.shape[0], ACTIONS))                         #32, 2
+
+            #Now we do the experience replay
+            for i in range(0, len(minibatch)):
+                state_t = minibatch[i][0]    # 4D stack of images
+                action_t = minibatch[i][1]   #This is action index
+                reward_t = minibatch[i][2]   #reward at state_t due to action_t
+                state_t1 = minibatch[i][3]   #next state
+                terminal = minibatch[i][4]   #wheather the agent died or survided due the action
+                
+
+                inputs[i:i + 1] = state_t    
+
+                targets[i] = model.predict(state_t)  # predicted q values
+                Q_sa = model.predict(state_t1)      #predict q values for next step
+                
+                if terminal:
+                    targets[i, action_t] = reward_t # if terminated, only equals reward
+                else:
+                    targets[i, action_t] = reward_t + GAMMA * np.max(Q_sa)
+
+            loss += model.train_on_batch(inputs, targets)
+            loss_df.loc[len(loss_df)] = loss
+        else:
+            # artificial time delay as training done with this delay
+            time.sleep(0.12)
+        s_t = initial_state if terminal else s_t1 #reset game to initial frame if terminate
+        t = t + 1
+        
+        # save progress every 1000 iterations
+        if t % 1000 == 0:
+            print("Now we save model")
+            
+            model.save_weights("model_final.h5", overwrite=True)
+            save_obj(D,"D") #saving episodes
+            save_obj(t,"time") #caching time steps
+            save_obj(epsilon,"epsilon") #cache epsilon to avoid repeated randomness in actions
+            loss_df.to_csv("./objects/loss_df.csv",index=False)
+            scores_df.to_csv("./objects/scores_df.csv",index=False)
+            actions_df.to_csv("./objects/actions_df.csv",index=False)
+            with open("model.json", "w") as outfile:
+                json.dump(model.to_json(), outfile)
+
+        # print info
+        state = ""
+        if t <= OBSERVE:
+            state = "observe"
+        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
+            state = "explore"
+        else:
+            state = "train"
+
+        print("TIMESTEP", t, "/ STATE", state,             "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,             "/ Q_MAX " , np.max(Q_sa), "/ Loss ", loss)
+
+    print("Episode finished!")
+    print("************************")
+
+
+def playGame(observe=False):
+    game = Game()
+    dino = DinoAgent(game)
+    game_state = Game_sate(dino,game)
+    model = buildmodel()
+    try:
+        trainNetwork(model,game_state,observe=observe)
+    except StopIteration:
+        game.end()
+
+def show_plots():
+    fig, axs = plt.subplots(ncols=1,nrows =2,figsize=(15,15))
+    axs[0].set_title('Loss')
+    axs[1].set_title('Game Score progress')
+    loss_df = pd.read_csv("./objects/loss_df.csv").clip(0,50).tail(100000)
+    scores_df = pd.read_csv("./objects/scores_df.csv").head(190000)
+    
+    actions_df = pd.read_csv("./objects/actions_df.csv").tail(100000)
+    loss_df['loss'] = loss_df['loss'].astype('float') 
+    loss_df.plot(use_index=True,ax=axs[0])
+    scores_df.plot(ax=axs[1])
+#     sns.distplot(actions_df,ax=axs[2])
+    imgg = fig.canvas.draw()
+
+#training_data_final_working.npy file contains the the keystrokes and gameframes recording for a score of 500
+supervised_frames = np.load("training_data_final_working.npy")
+frame = supervised_frames[0][0]
+action_index = supervised_frames[0][1]
+
+supervised_actions = []
+
+for frame in supervised_frames:
+    supervised_actions.append(frame[1])
+
+fig, axs = plt.subplots(ncols=1,nrows =2,figsize=(15,15))
+sns.distplot(supervised_actions,ax=axs[0])
+axs[1].set_title('AI gameplay distribution')
+axs[0].set_title('Human gameplay distribution')
+actions_df = pd.read_csv("./objects/actions_df.csv")
+sns.distplot(actions_df,ax=axs[1])
+
+
+
 
