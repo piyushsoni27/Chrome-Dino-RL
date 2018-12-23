433466ffda9677b8d22278e44259d2dcfa384cfa
piyushsoni27 <piy1995s@gmail.com>
2018-12-23 23:49:08 +0530

    added Game_state Class

--- game_module.py
+++ game_module.py
@@ -94,4 +94,28 @@ class DinoAgent:
     def duck(self):
         self._game.press_down()
 
+class Game_state:
+    def __init__(self,agent,game):
+        self._agent = agent
+        self._game = game
+        self._display = show_img() #display the processed image on screen using openCV, implemented using python coroutine 
+        self._display.__next__() # initiliaze the display coroutine 
+    def get_state(self,actions):
+        actions_df.loc[len(actions_df)] = actions[1] # storing actions in a dataframe
+        score = self._game.get_score() 
+        reward = 0.1*score/10 # dynamic reward calculation
+        is_over = False #game over
+        if actions[1] == 1:
+            self._agent.jump()
+            reward = 0.1*score/11
+        image = grab_screen() 
+        self._display.send(image) #display the image on screen
+
+        if self._agent.is_crashed():
+            scores_df.loc[len(loss_df)] = score # log the score when game is over
+            self._game.restart()
+            reward = -11/score
+            is_over = True
+        return image, reward, is_over #return the Experience tuple
+
 
