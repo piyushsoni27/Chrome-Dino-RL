4146398b68f4915992e72f9284522b2bc50e1d41
piyushsoni27 <piy1995s@gmail.com>
2018-12-23 23:46:04 +0530

    added files

--- game_module.py
+++ game_module.py
@@ -1,88 +1,97 @@
 import numpy as np
-from PIL import ImageGrab, Image
-import cv2
-import os
+from PIL import ImageGrab #grabbing image
+from PIL import Image
+import cv2 #opencv
+import io
 import time
-
+from matplotlib import pyplot as plt
+plt.rcParams['figure.figsize'] = (30, 30)
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from random import randint
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-
+#keras imports
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD , Adam
+from keras.callbacks import TensorBoard
+from collections import deque
+import random
+import pickle
+import json
 
 #path variables
 game_url = "game/dino.html"
-chrome_driver_path = "./chromedriver.exe"
-#loss_file_path = "./objects/loss_df.csv"
-#actions_file_path = "./objects/actions_df.csv"
-#scores_file_path = "./objects/scores_df.csv"
+chrome_driver_path = "../chromedriver.exe"
+loss_file_path = "./objects/loss_df.csv"
+actions_file_path = "./objects/actions_df.csv"
+scores_file_path = "./objects/scores_df.csv"
 
+'''
+* Game class: Selenium interfacing between the python and browser
+* __init__():  Launch the broswer window using the attributes in chrome_options
+* get_crashed() : return true if the agent as crashed on an obstacles. Gets javascript variable from game decribing the state
+* get_playing(): true if game in progress, false is crashed or paused
+* restart() : sends a signal to browser-javascript to restart the game
+* press_up(): sends a single to press up get to the browser
+* get_score(): gets current game score from javascript variables.
+* pause(): pause the game
+* resume(): resume a paused game if not crashed
+* end(): close the browser and end the game
+'''
 class Game:
-    
-    def __init__(self):
+    def __init__(self,custom_config=True):
         chrome_options = Options()
         chrome_options.add_argument("disable-infobars")
         self._driver = webdriver.Chrome(executable_path = chrome_driver_path,chrome_options=chrome_options)
         self._driver.set_window_position(x=-10,y=0)
         self._driver.set_window_size(200, 300)
         self._driver.get(os.path.abspath(game_url))
-    
-    def _isplaying(self):
-        return self._driver.execute_script("return Runner.instance_.playing")
-    
-    def _iscrashed(self):
+        #modifying game before trainNetworkining
+        if custom_config:
+            self._driver.execute_script("Runner.config.ACCELERATION=0")
+    def get_crashed(self):
         return self._driver.execute_script("return Runner.instance_.crashed")
-    
-    def _restart(self):
+    def get_playing(self):
+        return self._driver.execute_script("return Runner.instance_.playing")
+    def restart(self):
         self._driver.execute_script("Runner.instance_.restart()")
         
         time.sleep(0.25)# no actions are possible 
                         # for 0.25 sec after game starts, 
                         # skip learning at this time and make the model wait
-                        
-    def _pressUp(self):    
+    def press_up(self):
         self._driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_UP)
-        
-    def _pressDown(self):
-        self._driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_DOWN)
-    
-    def _get_score(self):
+    def get_score(self):
         score_array = self._driver.execute_script("return Runner.instance_.distanceMeter.digits")
         score = ''.join(score_array) # the javascript object is of type array with score in the formate[1,0,0] which is 100.
         return int(score)
-        
-    def _pause(self):
+    def pause(self):
         return self._driver.execute_script("return Runner.instance_.stop()")
-    
-    def _resume(self):
+    def resume(self):
         return self._driver.execute_script("return Runner.instance_.play()")
-    
-    def _end(self):
+    def end(self):
         self._driver.close()
-        
-        
-class Dino:
-    
-    def __init__(self):
-        self._game = Game()
-        self._jump()
-        time.sleep(0.5)
-        
-    def _jump(self):
-        self._game._pressUp()
-        
-    def _duck(self):
-        self._game._pressDown()
-        
-    def _isRunning(self):
-        return self._game._isPlaying()
-    
-    def _isCrashed(self):
-        return self._game._isCrashed()
-        
 
-if __name__ == "__main__":
-    dino = Dino()
-    dino._jump()
-    time.sleep(0.5)
-    dino._jump()
+class DinoAgent:
+    def __init__(self,game): #takes game as input for taking actions
+        self._game = game; 
+        self.jump(); #to start the game, we need to jump once
+        time.sleep(.5) # no action can be performed for the first time when game starts
+    def is_running(self):
+        return self._game.get_playing()
+    def is_crashed(self):
+        return self._game.get_crashed()
+    def jump(self):
+        self._game.press_up()
+    def duck(self):
+        self._game.press_down()
+
+
